708df926f60488651ea20fe452c853c00d38255c
Maltainos <rvwigualzynhoMaltainos@gmail.com>
2022-05-24 10:55:26 +0200

    first commit

---
+++ src/main/java/com/soccoriusapp/mvc/SoccoriusApplication.java
@@ -0,0 +1,13 @@
+package com.soccoriusapp.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccoriusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccoriusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soccoriusapp/mvc/SoccoriusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soccoriusapp.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccoriusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
